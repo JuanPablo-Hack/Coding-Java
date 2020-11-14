ec47b4b1922a00ca618c4a0670fb8633873d1118
juanpablodejesus <juanpablodejesus_figueroa@ucol.mx>
2020-11-14 00:00:52 -0600

    Me salve putos

--- DataIOTest.java
+++ DataIOTest.java
@@ -5,7 +5,7 @@
 // 15.99  13      Duke Juggling Dolls
 //
 // y luego leerlo y sacarlo por pantalla
-// NOTA: los datos numéricos deben escribirse como "números" y
+// NOTA: los datos numï¿½ricos deben escribirse como "nï¿½meros" y
 // no como cadenas de caracteres.
 // NOTA: los Strings deben escribirse como cadenas de bytes
 // no como cadenas de caracteres (1caracter = 2 bytes)
@@ -16,16 +16,16 @@ public class DataIOTest {
     public static void main(String[] args) throws IOException {
 
         //E/S con DataInputStream y DataOutputStream
-        //Conveniente con datos numéricos (stream = secuencia de bytes)
+        //Conveniente con datos numï¿½ricos (stream = secuencia de bytes)
         //Aconsejable para ficheros o sockets pero no para teclado y pantalla
 
         //EJERCICIO: Instancie un objeto de tipo DataOutputStream para escribir
-        //           en el fichero "invoice1.txt"
-        DataOutputStream out =
+        //           en el fichero "escribir.txt"
+        DataOutputStream out = new DataOutputStream(new FileOutputStream("escribir.txt"));
 
         //EJERCICIO: Instancie un objeto de tipo DataInputStream para leer
-        //           del fichero "invoice1.txt"
-        DataInputStream in =
+        //           del fichero "leer.txt"
+        DataInputStream in = new DataInputStream(new FileInputStream("leer.txt"));
 
         //Los datos son:
         double[] prices = { 19.99, 9.99, 15.99, 3.99, 4.99 };
@@ -56,10 +56,13 @@ public class DataIOTest {
         try {
             while (true) {
                 //EJERCICIO: leer el primer double del fichero sobre la variable price
+            	price = in.readDouble();
                 in.readChar();       // throws out the tab
                 //EJERCICIO: leer el int siguiente sobre la variable unit
+                unit = in.readInt();
                 in.readChar();       // throws out the tab
                 //EJERCICIO: leer la cadena siguiente sobre la variable desc
+                desc = in.readLine();
                 System.out.println("You've ordered " +
 				              unit + " units of " +
 				              desc + " at $" + price);
@@ -69,6 +72,6 @@ public class DataIOTest {
         }
         System.out.println("For a TOTAL of: $" + total);
         in.close();
-        while (true){};
+        while (true){}
     }
 }
